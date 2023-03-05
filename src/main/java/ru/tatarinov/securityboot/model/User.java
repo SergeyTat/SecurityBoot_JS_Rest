ae228266b9e83dce707ad87e1ebb8d294add254c
tatks <tatks@mail.ru>
2023-03-05 20:00:16 +0300

    05032023

--- src/main/java/ru/tatarinov/securityboot/model/User.java
+++ src/main/java/ru/tatarinov/securityboot/model/User.java
@@ -94,4 +94,26 @@ public class User implements UserDetails {
                 ", email='" + email + '\'' +
                 '}';
     }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+
 }
